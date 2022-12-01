44be257976495a131f7c3bc0081835109d557a3c
Eric Chin <ericychin94@gmail.com>
2022-12-01 12:13:16 -0800

    Middleware

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ let notes = [
   }
 ];
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+app.use(requestLogger);
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
@@ -77,6 +87,12 @@ app.post('/api/notes', (request, response) => {
   notes = notes.concat(note);
   response.json(note);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
