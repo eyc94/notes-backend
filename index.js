6905be1eb9fc490fda466fc93cec42e77e213e73
Eric Chin <ericychin94@gmail.com>
2022-12-01 14:29:08 -0800

    Serving static files from the backend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 let notes = [
   {
@@ -35,10 +36,6 @@ const requestLogger = (request, response, next) => {
 };
 
 app.use(requestLogger);
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
 
 app.get('/api/notes', (request, response) => {
   response.json(notes);
