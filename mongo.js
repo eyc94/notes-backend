106cffb255c6a8d73bc9ca30baa7cacc647b6543
Eric Chin <ericychin94@gmail.com>
2022-12-01 18:23:16 -0800

    Fetching objects from the database

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://<username>:<password>@cluster0.p4r1ead.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://admin:${password}@cluster0.p4r1ead.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -22,16 +22,22 @@ mongoose
   .then((result) => {
     console.log('connected!');
 
-    const note = new Note({
-      content: 'HTML is Easy',
-      date: new Date(),
-      important: true,
-    });
+    // const note = new Note({
+    //   content: 'HTML is Easy',
+    //   date: new Date(),
+    //   important: true,
+    // });
 
-    return note.save();
+    // return note.save();
+    Note.find({}).then(result => {
+      result.forEach(note => {
+        console.log(note);
+      });
+      mongoose.connection.close();
+    });
   })
-  .then(() => {
-    console.log('Note saved!');
-    return mongoose.connection.close();
-  })
+  // .then(() => {
+  //   console.log('Note saved!');
+  //   return mongoose.connection.close();
+  // })
   .catch((err) => console.log(err));
