c722eed6648eddb72c4759b30f807ef3f14445f6
Eric Chin <ericychin94@gmail.com>
2022-12-05 16:09:45 -0800

    Limiting creating new notes to logged in users

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
@@ -17,10 +18,24 @@ notesRouter.get('/:id', async (request, response) => {
   }
 });
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body;
-
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: 'Token missing or invalid',
+    });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
